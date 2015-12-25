0529a797a78ed84dc53e902dd2729fb888979b6b
muyz <yingzhi.mu@live.cn>
2015-12-25 14:14:41 +0800

    continue study

--- HelloWorld/httpReqeust.py
+++ HelloWorld/httpReqeust.py
@@ -1,5 +1,19 @@
 import urllib.request
 
 url = 'http://www.baidu.com/s?wd=cloga'
-html = urllib.request.urlopen(url).read()
+resp = urllib.request.urlopen(url)
+html = resp.read()
 print(html)
+print(resp.geturl())
+print(resp.info())
+try:
+	res = 2 / 0
+except:
+	import sys
+	tuple = sys.exc_info()
+	for i in tuple:
+		print(i)
+with open("helloworld.py") as f:
+	for line in f:
+		print(line)
+		
