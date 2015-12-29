d4339b55b13c39e41df0b68accc4b3a6b864bc04
muyz.york <yingzhi.mu@live.cn>
2015-12-29 10:30:26 +0800

    del test code

--- HelloWorld/helloworld.py
+++ HelloWorld/helloworld.py
@@ -7,24 +7,3 @@ import stocklib
 code = 'sz000503'
 stock = stocklib.readStock(code)
 print(str(stock))
-
-print ("您")
-
-
-
-class Fish(object):
-	"""docstring for Fish"""
-	def eat(self, food):
-		if food is not None:
-			self.hungry = False
-class User:
-	def __init__(myself,name):
-		myself.name = name
-	
-a = 10
-b = 8
-c = 9
-print( a % b)
-print( a // b)
-print(a>b<c)
-
